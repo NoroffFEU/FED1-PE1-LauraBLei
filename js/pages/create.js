198ec85991908f218961e9cabbc958ad2fdb8473
LauraBLei <146441897+LauraBLei@users.noreply.github.com>
2024-04-19 14:06:30 +0200

    created the forms

--- js/pages/create.js
+++ js/pages/create.js
@@ -10,16 +10,69 @@ const makePage = () => {
     headline.innerHTML = "Create Post"
 
     let imageBox = document.createElement("div")
+    imageBox.className = "addImageBox"
     
     let image = document.createElement("img")
+    image.className = "createPostAddImage"
 
     let addImage = document.createElement("img")
     addImage.src = "../pictures/addImage.png"
     addImage.alt = "Add Image button"
+    addImage.className = "addImageButton"
 
+    
+    
     main.appendChild(container)
     container.append(headline, imageBox)
     imageBox.append(image, addImage)
+    makeForms(container)
+}
+
+
+const makeForms = (container) => {
+let form = document.createElement("form");
+form.className = "flexContainerCenterCol form"
+
+let titleFormBox = document.createElement("div");
+titleFormBox.className = "flexContainerCenterRow"
+
+
+let titleLabel = document.createElement("label");
+titleLabel.innerHTML = "Title:";
+titleLabel.setAttribute("for", "title");
+titleLabel.className = "headerTwo"
+
+let titleInput = document.createElement("input");
+titleInput.className = "titleInput"
+titleInput.type = "text";
+titleInput.id = "title";
+titleInput.name = "title";
+titleInput.placeholder = "Insert text here..";
+
+let textFormBox = document.createElement("div");
+textFormBox.className = "flexContainerCenterRow"
+
+
+let textInput = document.createElement("textarea");
+textInput.className = "textInput"
+textInput.id = "text";
+textInput.name = "text";
+textInput.placeholder = "Add text here..";
+
+
+let submitButton = document.createElement("input");
+submitButton.type = "submit";
+submitButton.value = "Save";
+submitButton.className = "saveButton"
+
+let deleteButton = document.createElement("button")
+deleteButton.innerText = "Cancel"
+deleteButton.className = "deleteButton"
+
+container.append(form, deleteButton)
+form.append(titleFormBox, textFormBox, submitButton)
+titleFormBox.append(titleLabel,titleInput)
+textFormBox.append(textInput)
 }
 
 makePage()
