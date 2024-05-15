eafa8a4b18715c7f583454d67b9c5412f1d3bc04
LauraBLei <146441897+LauraBLei@users.noreply.github.com>
2024-05-15 12:22:00 +0200

    update location after creating post

--- js/pages/create.js
+++ js/pages/create.js
@@ -58,6 +58,7 @@ const makeForms = (container, imageBox) => {
     } else {
       imagePreview.src = "#";
     }
+    window.location.href = "../index.html"
   });
 
   let titleFormBox = document.createElement("div");
