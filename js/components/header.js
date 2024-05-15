7ec054a23f7e4be44c1c4e5b307ccb25664d18bc
LauraBLei <146441897+LauraBLei@users.noreply.github.com>
2024-05-15 10:05:30 +0200

    fixed header for phone and tablet

--- js/components/header.js
+++ js/components/header.js
@@ -3,13 +3,14 @@ export const makeHeader = () => {
   if (window.location.pathname.includes("post") || window.location.pathname.includes("account")) {
     isFrontPage = false
   }
-  desktopHeader(isFrontPage);
-  tabletHeader(isFrontPage);
-};
-
-const desktopHeader = (isFrontPage) => {
   let userInfo = JSON.parse(localStorage.getItem("userInfo"));
 
+  desktopHeader(isFrontPage, userInfo);
+  tabletHeader(isFrontPage, userInfo);
+};
+
+const desktopHeader = (isFrontPage, userInfo) => {
+  
   let header = document.querySelector("header");
 
   let container = document.createElement("div");
@@ -94,7 +95,7 @@ const desktopHeader = (isFrontPage) => {
   nav.append(createPost, home, logIn, logOut);
 };
 
-const tabletHeader = (isFrontPage) => {
+const tabletHeader = (isFrontPage, userInfo) => {
   let header = document.querySelector("header");
 
   let container = document.createElement("div");
@@ -107,6 +108,14 @@ const tabletHeader = (isFrontPage) => {
     logo.src = "../public/Logo.png";
   }
   logo.className = "cursor logo";
+  logo.addEventListener('click', () => {
+    if (isFrontPage) {
+      window.location.href = "index.html"
+
+    } else {
+      window.location.href = "../index.html"
+    }
+  })
 
   let details = document.createElement("details");
 
@@ -121,31 +130,69 @@ const tabletHeader = (isFrontPage) => {
   }
 
   let ul = document.createElement("ul");
-  ul.className = "boxMenu";
+  ul.className = "boxMenu flex justify-center items-center";
 
-  let createPost = document.createElement("li");
+  let createPost = document.createElement("a");
   createPost.innerText = "+ Create Post";
-  createPost.className = "headerTwo marginBotTop";
+  createPost.className = "headerTwo marginBotTop styles-none";
+  if (userInfo) {
+    createPost.style.display = "block"
+  } else {
+    createPost.style.display = "none"
+  }
+  if (isFrontPage) {
+    createPost.href = "post/create.html"
+  } else {
+    createPost.href = "../post/create.html"
+  }
 
-  let home = document.createElement("li");
+  let home = document.createElement("a");
   home.innerText = "Home";
-  home.className = "headerTwo marginBotTop";
+  home.className = "headerTwo marginBotTop styles-none";
+  if (isFrontPage) {
+    home.href = "index.html";
+  } else {
+    home.href = "../index.html";
+  }
 
-  let logIn = document.createElement("li");
+
+  let logOut = document.createElement("button");
+  logOut.innerText = "Log Out";
+  logOut.className = "headerText margin cursor"
+  logOut.className = "hidden smallBlueButton" 
+  logOut.onclick = () => {
+    localStorage.removeItem("userInfo")
+    location.reload()
+  }
+
+  let logIn = document.createElement("a");
   logIn.innerText = "Log In";
-  logIn.className = "headerTwo marginBotTop";
+  logIn.className = "headerTwo marginBotTop cursor styles-none";
+  if (isFrontPage) {
+    logIn.href = "account/login.html";
+  } else {
+    logIn.href = "../account/login.html";
+  }
 
-  let aboutUs = document.createElement("li");
+  if (userInfo) {
+    logIn.style.display = "none"
+    logOut.style.display = "block"
+  } else {
+    logIn.style.display = "block"
+    logOut.style.display = "none"
+  }
+
+  let aboutUs = document.createElement("a");
   aboutUs.innerText = "About Us";
   aboutUs.className = "headerTwo marginBotTop";
 
-  let sponsers = document.createElement("li");
+  let sponsers = document.createElement("a");
   sponsers.innerText = "Sponsers";
-  sponsers.className = "headerTwo marginBotTop";
+  sponsers.className = "headerTwo marginBotTop styles-none";
 
   header.appendChild(container);
   container.append(logo, details);
   details.append(menuButton, ul);
   menuButton.appendChild(menuImg);
-  ul.append(home, aboutUs, sponsers, logIn, createPost);
+  ul.append(home, aboutUs, sponsers, logIn, createPost, logOut);
 };
